f352a9eeb75a34f48de347acd56bb503d528b78f
amichalo421 <andre.michalowski@gmail.com>
2019-10-18 09:26:08 +0200

    array1

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -69,11 +69,20 @@ const graduates = [
   { id: 10, first_name: "Hube", university: "Universitat Rovira I Virgili Tarragona", email: "hlethbrig9@foxnews.com" },
 ];
 
-/* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
+/* Request 1: Create a new array called universities that contains all the universities in the graduates array. 
+This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities);
+const universitiesSort = () => { // A. B. 
+  const universities = []; // C. Array for sorted models
+  for (let i=0; i < graduates.length; i++) { // D. for loop
+    //create new new space to store generic values
+    let justSchools = graduates[i]['university']; // E. New statements // (inventory models listed)
+    universities.push(justSchools); // E. // (models pushed to array)
+    }
+    console.log(universities.sort()); // F. // (console log models // .sort fxn added
+}
+universitiesSort();
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
